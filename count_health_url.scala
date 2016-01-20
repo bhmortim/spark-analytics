bbfbf2e5338b16993a4cbd7615f17b306c9e54ee
Kevin Duraj <kevinduraj@gmail.com>
2016-01-19 21:55:24 -0800

    update

--- count_health_url.scala
+++ count_health_url.scala
@@ -1,8 +1,15 @@
 import org.apache.spark.sql.functions._
 
+// load engine visisted
 val df = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "visited" )).load
 
+// filter health within url
 val healthDF = df.filter(col("url").contains("health"))
 
-healthDF.take(100).foreach(println)
+// output sample on the screen
+healthDF.take(10).foreach(println)
+
+// Save output to health table
+healthDF.write.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "health" )).save
+
 
