1debe095ef7d731ca90456efe24c67ae209f42ef
root <root@video.attlocal.net>
2016-01-19 23:13:08 -0800

    create table

--- count_disease_url.scala
+++ count_disease_url.scala
@@ -1,20 +1,25 @@
 // Execute: dse spark -i:count_disease_url.scala
 import org.apache.spark.sql.functions._
 
-val disease = "gastrointestinal"
+val disease = "hepatitis"
+
+csc.setKeyspace("disease")
 
 // load engine visisted
-val df = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "visited" )).load
+val df1 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "visited" )).load
 
 // filter disease within url
-val diseaseDF = df.filter(col("url").contains(disease))
+val diseaseDF = df1.filter(col("url").contains(disease))
 
 // output sample on the screen
 // diseaseDF.take(10).foreach(println)
 
-// Save output to disease table
-diseaseDF.write.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> disease )).save
+// create table
+val table = csc.sql("CREATE TABLE IF NOT EXISTS " + disease + " ( url text PRIMARY KEY);")
 
-val df2 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> disease )).load
+// Save output to disease table
+diseaseDF.write.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> disease )).save
+
+val df2 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> disease )).load
 df2.count()
 
