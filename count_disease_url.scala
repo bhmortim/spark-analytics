94a5ef39913a33732e7b6ab283271cf784e13305
Kevin Duraj <kduraj@gmail.com>
2016-01-23 22:08:29 -0800

    find multiple diseases

--- count_disease_url.scala
+++ count_disease_url.scala
@@ -1,27 +1,24 @@
-// Execute: dse spark -i:count_disease_url.scala
+// Execute: 
+// dse spark -i:count_disease_url.scala
 import org.apache.spark.sql.functions._
 
+val diseases=Array("mental","headache","cardio","disease","heart","fungal","gerson","vulgaris")
+
 //-------------------------------------------------------------------------------------------------------------------------//
-val disease = "health"
 csc.setKeyspace("disease")
-csc.sql("CONSISTENCY ANY")
+//csc.sql("CONSISTENCY ANY")
 
 //-------------------------------------------------------------------------------------------------------------------------//
-// load engine visisted and filter disease within url
 val df1 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "visited" )).load
-val diseaseDF = df1.filter(col("url").contains(disease))
-// diseaseDF.take(10).foreach(println)
+
+diseases.foreach( disease => { 
+    val diseaseDF = df1.filter(col("url").contains(disease))
+    diseaseDF.write.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> disease )).save
+} )
 
 //-------------------------------------------------------------------------------------------------------------------------//
-// create table
-// val table = csc.sql("CREATE TABLE IF NOT EXISTS " + disease + " ( url text PRIMARY KEY);")
-
+// Count records
+//val df2 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> disease )).load
+//df2.count()
 //-------------------------------------------------------------------------------------------------------------------------//
-// Save output to disease table
-diseaseDF.write.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> disease )).save
-
-//-------------------------------------------------------------------------------------------------------------------------//
-// Count newly found records
-val df2 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> disease )).load
-df2.count()
 
