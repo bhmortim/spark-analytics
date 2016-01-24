eeb748a1394ba34bf7378197c6f59dc732cb5dfc
Kevin Duraj <kduraj@gmail.com>
2016-01-23 22:28:06 -0800

    parition diseases

--- partition-general-disease.scala
+++ partition-general-disease.scala
@@ -6,11 +6,11 @@ import org.apache.spark.sql.functions._
 val diseases=Array("mental","headache","cardio","disease","heart","fungal","gerson","vulgaris")
 
 //-------------------------------------------------------------------------------------------------------------------------//
+// Read Table from Keyspace into Data Frame
 csc.setKeyspace("disease")
-
-//-------------------------------------------------------------------------------------------------------------------------//
 val df1 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "visited" )).load
 
+//-------------------------------------------------------------------------------------------------------------------------//
 diseases.foreach( disease => {
     println(disease)
     val df2 = df1.filter(col("url").contains(disease))
