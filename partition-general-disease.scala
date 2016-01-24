57052224bcc7030d5f8effa87ab6c3989658e814
Kevin Duraj <kduraj@gmail.com>
2016-01-23 23:43:13 -0800

    general diseases

--- partition-general-disease.scala
+++ partition-general-disease.scala
@@ -1,26 +1,54 @@
 //-------------------------------------------------------------------------------------------------------------------------//
 // Execute: Spark Job 
-// dse spark -i:count_disease_url.scala
+// dse spark -i:partition-general-disease.scala 
 import org.apache.spark.sql.functions._
 
-val diseases=Array("mental","headache","cardio","disease","heart","fungal","gerson","vulgaris")
+val diseases=Array(
+"carcinoma"
+,"treatment"
+,"syndrome"
+,"tumor"
+,"liver"
+,"lung"
+,"gastric"
+,"tuberculosis"
+,"surgery"
+,"genetics"
+,"chromozone"
+,"health"  
+,"cancer"  
+,"hepatitis"
+,"brain"    
+,"biology"
+,"mental"   
+,"headache" 
+,"cardio"   
+,"disease"  
+,"heart"    
+,"fungal"   
+,"gerson"  
+,"vulgaris"
+)
+                    )
 
 //-------------------------------------------------------------------------------------------------------------------------//
 // Read Table from Keyspace into Data Frame
 csc.setKeyspace("disease")
 val df1 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "engine", "table" -> "visited" )).load
-
+df1.printSchema()
 //-------------------------------------------------------------------------------------------------------------------------//
 diseases.foreach( disease => {
+
     println(disease)
     val df2 = df1.filter(col("url").contains(disease))
-    val df3 = diseaseDF.withColumn("condition", disease ) 
+    val df3 = df2.withColumn("condition", org.apache.spark.sql.functions.lit(disease))
+    //df3.printSchema() //df3.show()
     df3.write.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> "general" )).save
 } )
 
 //-------------------------------------------------------------------------------------------------------------------------//
 // Count records
-val df4 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> "general" )).load
-df4.count()
+//val df4 = csc.read.format("org.apache.spark.sql.cassandra").options(Map( "keyspace" -> "disease", "table" -> "general" )).load
+//df4.count()
 //-------------------------------------------------------------------------------------------------------------------------//
 
